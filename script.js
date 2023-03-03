17ff34040b42f639fd8c8db8443246bd84ab93b8
Climax Mba <climaxium150@gmail.com>
2023-03-03 14:16:59 +0100

    Modify files for heading & button

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const container = document.getElementById('container');
 
 let dim = 16;
+
 for (let i = 1; i <= dim * dim; i++) {
-    container.innerHTML += '<div class="grid-items"></div>';
+    const div = document.createElement('div');
+    container.appendChild(div);
+    div.addEventListener('mouseover', () => div.style.backgroundColor = "black");
 }
 
-container.style.cssText = `grid-template-columns: repeat(${dim}, auto)`;
+container.style.gridTemplateColumns = `repeat(${dim}, auto)`;
 let grids = container.childNodes;
+document.querySelector('button').onclick = promptDim;
 
-for (let i = 0; i < grids.length; i++) {
-    grids[i].addEventListener('mouseover', () => grids[i].style.backgroundColor = 'black');
+function promptDim() {
+    let temp = dim;
+    dim = 0;
+    do {
+        dim = Number(prompt('Enter a number from 4 to 100 to change dimension', `${temp}`));
+        if (!dim) {
+            dim = temp;
+            return;
+        }
+    } while ((dim < 4) || (dim > 100))
+    setDim();
 }
 
-// Set up user input popup for grid dimension. #4
+function setDim() {
+    for (let i = 0; i < grids.length; i++) {
+        grids[i].style.backgroundColor = 'unset';
+    }
+    container.innerHTML = '';
+    for (let i = 1; i <= dim * dim; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        div.addEventListener('mouseover', () => div.style.backgroundColor = "black");
+    }
+    container.style.gridTemplateColumns = `repeat(${dim}, auto)`;
+}
 
 // Extra
