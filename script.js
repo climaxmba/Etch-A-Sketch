28d91c9d422a238106370a31590e0bf88fc372bf
Climax Mba <climaxium150@gmail.com>
2023-03-03 05:27:11 +0100

    Link files, add grid-items in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Select container
+const container = document.getElementById('container');
+
+// Loop to create required amount of grids
+let dim = 16;
+for (let i = 0; i < dim; i++) {
+    container.innerHTML += '<div class="grid-items"></div>';
+}
+
+// Add grids to container
+
+// Add CSS properties to grid
+
+// Set a hover effect. #3
+
+// Set up user input popup for grid dimension. #4
+
+// Extra
